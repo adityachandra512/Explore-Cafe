e16acf541bc4b5a32bbfd94b6b811821bf724f57
adityachandra512 <143398217+adityachandra512@users.noreply.github.com>
2024-07-18 11:40:20 +0530

    Update footer.jsx

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -49,7 +49,7 @@ function footer() {
     </div>
   </nav>
   <aside>
-    <p>Copyright © ${new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © ${new Date().getFullYear()} - All right reserved by Explore Cafe Ltd</p>
   </aside>
 </footer> 
     </div>
